d6a7cade85c4dfcf462b56932d14e7edcae550ca
Stephan Hoyer <ste.hoyer@gmail.com>
2012-10-11 21:34:54 +0200

    Add config route and draw paddles

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
 var restify = require('restify');
 
-var FIELD_HEIGHT = 300;
-var FIELD_WIDTH = 300;
+var config = {
+  FIELD_HEIGHT: 300,
+  FIELD_WIDTH: 300,
 
-var TIME_QUANTUM = 10;
-var INITIAL_BALL_SPEED = 1;
+  FIELD_PADDING_X: 10,
+  FIELD_PADDING_Y: 10,
+
+  PADDLE_HEIGHT: 70,
+  PADDLE_WIDTH: 20,
+
+  TIME_QUANTUM: 10,
+  INITIAL_BALL_SPEED: 5
+};
 
 function random(value) {
   return Math.random() * value * 2 - value;
 }
 
 var game = {
-  ball: [0, 0],
+  ball: [config.FIELD_WIDTH / 2, config.FIELD_HEIGHT /2],
   ballDelta: [0, 0],
-  paddleLeft: 0,
-  paddleRight: 0,
+  paddleLeft: config.FIELD_HEIGHT / 2,
+  paddleRight: config.FIELD_HEIGHT / 2,
   start: function() {
-    this.ball = [FIELD_WIDTH / 2, FIELD_HEIGHT /2];
-    this.ballDelta = [random(INITIAL_BALL_SPEED), random(INITIAL_BALL_SPEED)];
+    this.ball = [config.FIELD_WIDTH / 2, config.FIELD_HEIGHT /2];
+    this.ballDelta = [random(config.INITIAL_BALL_SPEED), random(config.INITIAL_BALL_SPEED)];
     setTimeout(this.run.bind(this), 100);
   },
   run: function() {
-    if (this.ball[0] >= FIELD_HEIGHT || this.ball[0] <= 0) {
+    if (this.ball[0] >= config.FIELD_WIDTH - config.FIELD_PADDING_X) {
+      if (this.ball[1] > this.paddleRight - config.PADDLE_HEIGHT/2 || 
+          this.ball[1] < this.paddleRight + config.PADDLE_HEIGHT/2) {
+        this.ballDelta[0] *= -1;
+      }
+    }
+
+    if(this.ball[0] <= config.FIELD_PADDING_X) {
       this.ballDelta[0] *= -1;
     }
-    if (this.ball[1] >= FIELD_HEIGHT || this.ball[1] <= 0) {
-      this.ballDelta[1] *= -1;
-    }
+
+    if (this.ball[1] >= config.FIELD_HEIGHT - config.FIELD_PADDING_Y || 
+        this.ball[1] <= config.FIELD_PADDING_Y) {
+          this.ballDelta[1] *= -1;
+        }
     this.ball[0] += this.ballDelta[0];
     this.ball[1] += this.ballDelta[1];
-    setTimeout(this.run.bind(this), TIME_QUANTUM);
+    setTimeout(this.run.bind(this), config.TIME_QUANTUM);
     console.log(this.ball);
   }
 };
@@ -39,13 +56,18 @@ function startGame(req, res, next) {
   res.send('game started');
 }
 
-function status(req, res, next) {
+function getConfig(req, res, next) {
+  res.send(config);
+}
+
+function getStatus(req, res, next) {
   res.send(game);
 }
 
 var server = restify.createServer();
+server.get('/game/config/', getConfig);
 server.get('/game/start/', startGame);
-server.get('/game/status/', status);
+server.get('/game/status/', getStatus);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);

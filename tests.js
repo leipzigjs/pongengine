194934e2094f06587bf4f70a1fcf5ae445f6e21d
Stephan Hoyer <ste.hoyer@gmail.com>
2012-11-01 21:19:42 +0100

    Add tests for game

---
+++ tests.js
@@ -0,0 +1,50 @@
+var expect = require('chai').expect;
+var Game = require('./game');
+
+
+describe('Game', function() {
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+ 
+  it('should create a Game object', function() {
+    expect(game).to.be.object;
+  });
+
+  it('should be able to log in', function() {
+    expect(game).to.respondTo('loginPlayer');
+    var playername = 'foo';
+    var player = game.loginPlayer(playername);
+    expect(player).to.be.a('object');
+    expect(player).to.have.property('name');
+    expect(player).to.have.property('secret');
+    expect(player.secret).to.be.a('string').with.length.above(2);
+    expect(game).to.have.property('players');
+    expect(game.players).to.have.property('left');
+    expect(game.players.left).to.have.property('name');
+    expect(game.players.left.name).to.equal(playername);
+  });
+
+  it('should only allow 2 players to log in', function() {
+    expect(game.loginPlayer('one')).to.be.ok; 
+    expect(game.loginPlayer('two')).to.be.ok; 
+    expect(function() {
+      game.loginPlayer('three');
+    }).to.throw.error;
+  });
+
+  it('should start the game, if two players log in', function(done) {
+    game.config.WAIT_BEFORE_START = 10
+    expect(game).to.have.property('status');
+    expect(game.status).to.equal('login');
+    game.loginPlayer('left');
+    game.loginPlayer('right');
+    expect(game.status).to.equal('ready');
+    setTimeout(function() {
+      expect(game.status).to.equal('started');
+      done();
+    }, 20);
+  });
+});
